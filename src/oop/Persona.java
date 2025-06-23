59bdef65368b69599ce63012549647b7b19800de
Elvis Almonte <elvisalmonte14@gmail.com>
2025-06-22 21:21:19 -0400

    creacion de la clase persona y el metodo imprimirInformacion

---
+++ src/oop/Persona.java
@@ -0,0 +1,20 @@
+package oop;
+
+//Definicion de la clase
+public class Persona {
+    //atributos
+    String nombre;
+    int edad;
+    char genero;
+
+    //metodos
+    void imprimirInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Genero: " + genero);
+    }
+
+
+
+
+}
