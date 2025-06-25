6bd683b87342c049218c0a37aaf7cab09a00700a
Elvis Almonte <elvisalmonte14@gmail.com>
2025-06-24 22:34:54 -0400

    Operador instanceof

--- src/oop/herencia/EjemploHerencia.java
+++ src/oop/herencia/EjemploHerencia.java
@@ -21,12 +21,32 @@ class Gato extends Animal{
 
 }
 
+class Puerco extends Animal{
+
+}
+
+class Carro{
+
+}
+
+
+
 public class EjemploHerencia{
     public static void main(String[] args) {
+        Carro carro = new Carro();
+        Animal animal = new Animal();
         Perro perro = new Perro();
         perro.nombre = "Tobty";
         perro.respirar();
         perro.comer();
+        Puerco puerco = new Puerco();
+        puerco.nombre = "Choin";
+        puerco.respirar();
+        System.out.println(perro instanceof Animal);
+        System.out.println(perro instanceof Perro);
+        System.out.println(puerco instanceof Puerco);
+        System.out.println(animal instanceof Puerco);
+
     }
 }
 
